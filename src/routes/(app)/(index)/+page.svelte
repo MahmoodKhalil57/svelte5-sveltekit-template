7a05dcab7b37fd7fd45b4f85d9dab3d92e05b9c6
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2024-05-04 23:56:37 +0100

    cleanup layout

--- src/routes/(app)/(index)/+page.svelte
+++ src/routes/(app)/(index)/+page.svelte
@@ -1,3 +1,3 @@
-<div class="w-full flex flex-col justify-center items-center">
+<section class="w-full flex flex-col justify-center items-center">
 	<div class="flex-col gap-6 flex-center">Hello</div>
-</div>
+</section>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -3,14 +3,8 @@
 	import Header from '$lib/components/layout/header.svelte';
 </script>
 
-<div class="w-full min-h-screen-new">
-	<div class="w-full min-h-header">
-		<Header />
-	</div>
-	<div class="flex justify-center stretch min-h-page">
-		<slot />
-	</div>
-	<div class="w-full min-h-footer">
-		<Footer />
-	</div>
-</div>
+<Header />
+<main class="flex justify-center stretch min-h-page">
+	<slot />
+</main>
+<Footer />
