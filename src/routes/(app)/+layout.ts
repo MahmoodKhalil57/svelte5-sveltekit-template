2c5b7358fb0e26a2c4499093c66a30d1d2d1e869
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2024-05-05 16:14:16 +0100

    remove unused

--- src/routes/(app)/+layout.ts
+++
@@ -1,6 +0,0 @@
-import { serverStoreHandle } from '$lib/stores/serverStoreHandle';
-import type { LayoutLoad } from './$types';
-
-export const load: LayoutLoad = async ({ data }) => {
-	serverStoreHandle.userAttributes.set(data?.userAttributes ?? null);
-};
