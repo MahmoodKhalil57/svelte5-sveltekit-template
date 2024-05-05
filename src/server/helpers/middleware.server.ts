dbab25ba397f894268882957eb1b4e0bfdb0df33
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2024-05-05 11:52:29 +0100

    cleanup logger identifier

--- src/server/helpers/middleware.server.ts
+++ src/server/helpers/middleware.server.ts
@@ -3,12 +3,18 @@ import { throwError } from '$apiUtils/server/apiUtils.server';
 import type { MiddlewareMap } from '$apiUtils/server/ApiUtils.type.server';
 import type { getContext } from '$api/helpers/context.server';
 import { dev } from '$app/environment';
+import { logData } from '$api/helpers/logger.server';
 
 export const middlewareMap = {
 	devProcedure: async function (ctx) {
 		if (!dev) {
 			throwError(responseStatus.FORBIDDEN);
 		}
+
+		logData.info({
+			codeLocation: '🚀 ~ hybridUser:',
+			identifier: 'developer'
+		});
 		return { ctx };
 	}
 } satisfies MiddlewareMap<Awaited<ReturnType<typeof getContext>>>;
